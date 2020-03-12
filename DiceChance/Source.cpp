1b5d393b13495b62228a1bb345993d23972c6c2b
alexkarate <alexkarate.s@mail.ru>
2020-03-12 20:47:17 +0500

    Rename variables and fix a bug

--- DiceChance/Source.cpp
+++ DiceChance/Source.cpp
@@ -5,32 +5,32 @@
 using namespace std;
 using namespace Dice;
 
-// Returns the number of digits
-int GetNumberOfDigits(int a) {
-	int k = 0;
-	while (a > 0) {
-		a /= 10; k++;
+// Returns the amount of digits in a number
+int GetNumberOfDigits(int number) {
+	int digits = 0;
+	while (number > 0) {
+		number /= 10; digits++;
 	}
-	return k;
+	return digits;
 }
 
 // Deletes spaces from the front and the back of the string
-void CleanString(std::string &s) {
-	int n = 0;
-	while (n < s.length() && (s[n] == ' ' || s[n] == '\t')) n++;
-	s.erase(0, n);
-	n = s.length() - 1;
-	while (n >= 0 && (s[n] == ' ' || s[n] == '\t')) n--;
-	s.erase(n + 1);
+void CleanString(string &str) {
+	int count = 0;
+	while (count < str.length() && (str[count] == ' ' || str[count] == '\t')) count++;
+	str.erase(0, count);
+	count = str.length() - 1;
+	while (count >= 0 && (str[count] == ' ' || str[count] == '\t')) count--;
+	str.erase(count + 1);
 }
-// Returns stoi(s) or defaultNum
-int SafeStoi(std::string &s, int defaultNum) {
+// Returns stoi(str) or defaultNumber
+int SafeStoi(string &str, int defaultNumber) {
 	int result;
 	try {
-		result = stoi(s);
+		result = stoi(str);
 	}
-	catch (const std::exception e) {
-		result = defaultNum;
+	catch (const exception e) {
+		result = defaultNumber;
 	}
 	return result;
 }
@@ -41,70 +41,70 @@ bool isDigit(char a) {
 }
 
 // Returns the type of comparison that is used and the number to compare to
-Dice::VsType GetType(string &s, int &vsNum) {
-	VsType vsType = NoVs;
-	vsNum = s.find('=');
-	if (vsNum != -1) vsType = Equals;
+VersusType GetType(string &str, int &versusNumber) {
+	VersusType versusType = NoVersus;
+	versusNumber = str.find('=');
+	if (versusNumber != -1) versusType = Equals;
 	else {
-		vsNum = s.find('>');
-		if (vsNum != -1) vsType = GreaterThan;
+		versusNumber = str.find('>');
+		if (versusNumber != -1) versusType = GreaterThan;
 		else {
-			vsNum = s.find('<');
-			if (vsNum != -1) vsType = LessThan;
+			versusNumber = str.find('<');
+			if (versusNumber != -1) versusType = LessThan;
 		}
 	}
-	if (vsType != NoVs) {
-		string vsStr = s.substr(vsNum + 1);
-		vsNum = SafeStoi(vsStr, -1);
+	if (versusType != NoVersus) {
+		string versusStr = str.substr(versusNumber + 1);
+		versusNumber = SafeStoi(versusStr, -1);
 	}
-	else vsNum = 0;
-	return vsType;
+	else versusNumber = 0;
+	return versusType;
 }
 
 // Returns the number of cubes in an expression or -1 if none
-int GetCubes(string &s, int dPos) {
-	string cubeStr = s.substr(0, dPos);
+int GetCubes(string &str, int dicePos) {
+	string cubeStr = str.substr(0, dicePos);
 	CleanString(cubeStr);
 	return SafeStoi(cubeStr, -1);
 }
 
 // Returns the number of sides in an expression or -1 if none
-int GetSides(string &s, int dPos) {
-	int n = 0;
-	while (s[dPos + n + 1] == ' ' || isDigit(s[dPos + n + 1])) n++;
-	string sideStr = s.substr(dPos + 1, n);
+int GetSides(string &str, int dicePos) {
+	int count = 0;
+	while (str[dicePos + count + 1] == ' ' || isDigit(str[dicePos + count + 1])) count++;
+	string sideStr = str.substr(dicePos + 1, count);
 	CleanString(sideStr);
 	return SafeStoi(sideStr, -1);
 }
 
 // Returns the addition (or subtraction) in an expression, or 0 if none
-int GetPlus(string &s) {
-	int op = s.find('+');
-	if (op == -1) op = s.find('-');
+int GetPlus(string &str) {
+	int op = str.find('+');
+	if (op == -1) op = str.find('-');
 	if (op == -1) return 0;
 
-	int n = 1;
-	while (s[op + n] == ' ' || isDigit(s[op + n])) n++;
-	string opStr = s.substr(op, n);
+	int count = 1;
+	while (str[op + count] == ' ' || isDigit(str[op + count])) count++;
+	string opStr = str.substr(op, count);
 	CleanString(opStr);
 	return SafeStoi(opStr, 0);
 }
 
 int main() {
-	int cubes, sides, plus, vsNum;
-	string s;
+	int cubes, sides, plus, versusNumber;
+	string str;
 	bool sucess;
-	VsType vsType;
+	VersusType versusType;
 	cout << "Please input the expression: ";
 	do {
-		getline(cin, s);
-		CleanString(s);
-		int dicePos = s.find('d');
-		cubes  = GetCubes(s, dicePos);
-		sides  = GetSides(s, dicePos);
-		vsType = GetType(s, vsNum);
-		plus   = GetPlus(s);
-		sucess = dicePos != -1 && cubes > 0 && sides > 0 && vsNum >= 0;
+		getline(cin, str);
+		CleanString(str);
+		int dicePos = str.find('d');
+		cubes  = GetCubes(str, dicePos);
+		sides  = GetSides(str, dicePos);
+		versusType = GetType(str, versusNumber);
+		plus   = GetPlus(str);
+		sucess = dicePos != -1 && cubes > 0 && sides > 0 && versusNumber >= 0;
 		if (!sucess) cout << "Invalid input. Please try again: ";
 	} while (!sucess);
 	int N;
@@ -112,12 +112,13 @@ int main() {
 	a = GetValueSpread(cubes, sides, N);
 	AdjustArray(a, N, plus);
 	double *chances = GetChanceArray(a, N);
-	double chance = GetChance(a, N, vsNum, vsType);
-	if (vsType)
+	double chance = GetChance(a, N, versusNumber, versusType);
+	if (versusType)
 		cout << fixed << chance * 100 << '%' << endl;
-	for (int i = 1; i < N; i++)
+	int idWidth = GetNumberOfDigits(N);
+	for (int i = 0; i < N; i++)
 		if(chances[i]>0)
-			cout << setw(GetNumberOfDigits(N)) << i << ' ' << fixed << chances[i] * 100  << '%'<< endl;
+			cout << setw(idWidth) << i << ": " << fixed << chances[i] * 100  << '%' << endl;
 	if (abs(chances[0] - 1) < 0.00001) cout << "No results";
 	delete[] a;
 	cin.get();
--- DiceChanceLibrary/DiceChance.h
+++ DiceChanceLibrary/DiceChance.h
@@ -1,24 +1,24 @@
 #pragma once
 namespace Dice{
-	enum VsType {
-		NoVs, GreaterThan, LessThan, Equals
+	enum VersusType {
+		NoVersus, GreaterThan, LessThan, Equals
 	};
 	// Returns a match array, or an array, where each position corresponds to the number of possible dice throws, 
 	// the result of which equals to that position.
-	int* GetValueSpread(int cubes, int sides, int &N);
+	int* GetValueSpread(int cubes, int sides, int &NResult);
 
 	// Returns the number of all matches
-	double GetTotal(int *a, int N);
+	double GetTotal(int *matchArray, int N);
 
 	// Returns a chance array, given a match array
-	double* GetChanceArray(int *a, int N);
+	double* GetChanceArray(int *matchArray, int N);
 
 	// Returns the chance, given a match array, the type of comparison and a number to compare to
-	double GetChance(int *a, int N, int vsNum, VsType type);
+	double GetChance(int *matchArray, int N, int vsNum, VersusType type);
 
 	// Adds two match arrays, the result is a match array
-	int* AddArray(int *a, int Na, int *b, int Nb, int& N);
+	int* AddArray(int *matchArrayA, int Na, int *matchArrayB, int Nb, int& NResult);
 
 	// Shifts the array to the right (or left) by 'plus'. Values with position less than 0 are kept in 0
-	void AdjustArray(int *&a, int &N, int plus);
+	void AdjustArray(int *&matchArray, int &N, int plus);
 }
